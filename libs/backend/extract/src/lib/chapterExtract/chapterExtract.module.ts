8ac63781859e4409db6f46420ac00a9bfd3d698a
Aderval <aderval758@gmail.com>
2022-05-27 16:46:15 -0300

    o módulo page extract não é mais importado por chapterExrtact

--- libs/backend/extract/src/lib/chapterExtract/chapterExtract.module.ts
+++ libs/backend/extract/src/lib/chapterExtract/chapterExtract.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PageExtractModule } from '../pageExtract/pageExtract.module';
 import { ChapterResolverExtract } from './chapterExtract.resolver';
 import { ChapterServiceExtract } from './chapterExtract.service';
 import { PuppeteerConfigModule } from '../PuppeteerConfig/puppeteerConfig.module';
 
 @Module({
-  imports: [PageExtractModule, PuppeteerConfigModule],
+  imports: [PuppeteerConfigModule],
   exports: [ChapterServiceExtract, ChapterResolverExtract],
   providers: [ChapterResolverExtract, ChapterServiceExtract],
 })
