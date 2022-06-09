9eb163f06c08430a143a416edf33750f38944764
Aderval <aderval758@gmail.com>
2022-06-09 14:59:01 -0300

    remove backend-extract.module.ts

--- libs/backend/extract/src/index.ts
+++ libs/backend/extract/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/backend-extract.module';
+export * from './lib/mangaExtract/mangaExtract.module';
+export * from './lib/mangaExtract/mangaExtract.resolver';
+export * from './lib/chapterExtract/chapterExtract.module';
+export * from './lib/chapterExtract/chapterExtract.resolver';
+export * from './lib/pageExtract/pageExtract.module';
+export * from './lib/pageExtract/pageExtract.resolver';
--- libs/backend/extract/src/lib/backend-extract.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MangaExtractModule } from './mangaExtract/mangaExtract.module';
-import { ChapterExtractModule } from './chapterExtract/chapterExtract.module';
-import { PageExtractModule } from './pageExtract/pageExtract.module';
-
-@Module({
-  exports: [MangaExtractModule, ChapterExtractModule, PageExtractModule],
-})
-export class BackendExtractModule {}
